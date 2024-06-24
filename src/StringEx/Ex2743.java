234a6e661b2afccf7561f76822baccf3f4882fe9
beombeom1 <160469902+beombeom1@users.noreply.github.com>
2024-06-24 15:52:26 +0900

    0624 study

---
+++ src/StringEx/Ex2743.java
@@ -0,0 +1,11 @@
+package StringEx;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Ex2743 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("HI");
+    }
+}
