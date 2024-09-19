4b9ed5c7da5b930b2980764cf62a0db031394394
beombeom1 <jebeam3632@naver.com>
2024-09-19 14:03:42 +0900

    ex9086

--- src/StringEx/Ex9086.java
+++ src/StringEx/Ex9086.java
@@ -4,11 +4,15 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Ex2743 {
+public class Ex9086 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s = br.readLine();
-        System.out.println(s.length());
+        int i = Integer.parseInt(br.readLine());
+
+        for (int j = 0; j < i; j++){
+            String s = br.readLine();
+            System.out.println(s.charAt(0) + "" + s.charAt(s.length() - 1));
+        }
 
     }
 }
