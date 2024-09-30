4d0e285ea9dac8d2509be50797010667e40f5cc2
beombeom1 <jebeam3632@naver.com>
2024-09-30 22:05:05 +0900

    ex14581

---
+++ src/Ex/Ex14581.java
@@ -0,0 +1,16 @@
+package Ex;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ex14581 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        System.out.println(":fan::fan::fan:");
+        System.out.println(":fan:" + ":" + s + ":" +  ":fan:");
+        System.out.println(":fan::fan::fan:");
+    }
+
+}
