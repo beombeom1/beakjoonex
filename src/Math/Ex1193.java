e5b8dbc76657db2e61b7b9ac7c5a0944b985dfdb
beombeom1 <jebeam3632@naver.com>
2024-10-10 09:44:30 +0900

    Ex1193

---
+++ src/Math/Ex1193.java
@@ -0,0 +1,24 @@
+package Math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Ex1193 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int x = Integer.parseInt(br.readLine());
+        int i = 1;
+
+        while (x > i) {
+            x -= i;
+            i++;
+        }
+
+        if (i % 2 == 0) {
+            System.out.println(x + "/" + (i - x + 1));
+        } else {
+            System.out.println((i - x + 1) + "/" + x);
+        }
+    }
+}
