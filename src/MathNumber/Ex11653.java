880b2f73a261658a5cc9023116a6b6af1c3029ce
beombeom1 <jebeam3632@naver.com>
2024-10-10 14:54:08 +0900

    Ex11653

---
+++ src/MathNumber/Ex11653.java
@@ -0,0 +1,10 @@
+package MathNumber;
+
+import java.util.Scanner;
+
+public class Ex11653 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int s = sc.nextInt();
+    }
+}
