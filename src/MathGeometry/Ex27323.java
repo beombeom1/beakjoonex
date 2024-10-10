bd8fd866d779ffd281bbe2bdd7506719d1b7184d
beombeom1 <jebeam3632@naver.com>
2024-10-10 15:51:00 +0900

    Ex27323

---
+++ src/MathGeometry/Ex27323.java
@@ -0,0 +1,12 @@
+package MathGeometry;
+
+import java.util.Scanner;
+
+public class Ex27323 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+        System.out.println(a * b);
+    }
+}
