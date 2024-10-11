f015b2f271fe4b12e73c97dba8250217671a1c3d
beombeom1 <jebeam3632@naver.com>
2024-10-11 09:24:51 +0900

    Ex15894

---
+++ src/MathGeometry/Ex15894.java
@@ -0,0 +1,11 @@
+package MathGeometry;
+
+import java.util.Scanner;
+
+public class Ex15894 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        long aa = s.nextLong();
+        System.out.println(aa * 4);
+    }
+}
