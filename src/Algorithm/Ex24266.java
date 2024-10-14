bfe1aa56ca64adc8be8cdc40589a288a227a7299
beombeom1 <jebeam3632@naver.com>
2024-10-14 13:52:18 +0900

    Ex24266

---
+++ src/Algorithm/Ex24266.java
@@ -0,0 +1,13 @@
+package Algorithm;
+
+import java.util.Scanner;
+
+public class Ex24266 {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    long s = sc.nextLong();
+
+    System.out.println(s*s*s);
+    System.out.println(3);
+    }
+}
